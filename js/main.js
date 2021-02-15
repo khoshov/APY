d660cc444bc43a8a695f22ef9a39b96b38ec9a95
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2021-02-15 13:47:37 +0400

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const calculateButton = document.querySelector('#calculate');
+
+calculateButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const principalInput = document.querySelector('#principal');
+    const interestInput = document.querySelector('#interest');
+    const daysInput = document.querySelector('#days');
+    const output = document.querySelector('#output')
+    const details = document.querySelector('#details')
+
+    const principal = new Decimal(principalInput.value);
+    const interest = new Decimal(interestInput.value);
+    const days = new Decimal(daysInput.value);
+    const daysInYear = new Decimal(365);
+
+    const result = ((1 + (interest / principal) / daysInYear) ** days - 1) * 100;
+    details.innerHTML = `((1 + (${interest} / ${principal}) / ${daysInYear}) ** ${days} - 1) * 100`;
+    output.innerHTML = `${result}`;
+});
